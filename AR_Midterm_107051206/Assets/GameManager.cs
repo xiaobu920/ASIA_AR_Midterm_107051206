567897703790646578f847d406a09c81f9b7ad31
xiaobu920 <71679970+xiaobu920@users.noreply.github.com>
2020-11-15 12:42:26 +0800

    15

---
+++ AR_Midterm_107051206/Assets/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("太空人")]
+    public Transform play1;
+    [Header("雞")]
+    public Transform play2;
+
+    private void Start()
+    {
+        print("開始事件執行中");
+    }
+    private void Update()
+    {
+        print("更新事件");
+        
+
+        
+    }
+}
+
