c329d34ba8a3b2754b523afed55271467d8daddd
xiaobu920 <71679970+xiaobu920@users.noreply.github.com>
2020-11-17 14:14:27 +0800

    最後一版

--- AR_Midterm_107051206/Assets/GameManager.cs
+++ AR_Midterm_107051206/Assets/GameManager.cs
@@ -4,12 +4,14 @@ public class GameManager : MonoBehaviour
 {
     [Header("太空人")]
     public Transform play1;
-    [Header("雞")]
-    public Transform play2;
+    [Header("女人")]
+    public Transform play3;
+
     [Header("太空人動畫元件")]
     public Animator aniplay1;
-    [Header("雞動畫元件")]
-    public Animator aniplay2;
+    [Header("女人動畫元件")]
+    public Animator aniplay3;
+
 
 
     private void Start()
@@ -26,20 +28,20 @@ public class GameManager : MonoBehaviour
     {
         print("跑步");
         aniplay1.SetTrigger("跑步");
-        aniplay2.SetTrigger("跑步");
+        aniplay3.SetTrigger("跑步");
     }
     public void walk()
     {
         print("走路");
         aniplay1.SetTrigger("走路");
-        aniplay2.SetTrigger("走路");
+        aniplay3.SetTrigger("走路");
 
     }
     public void PlayAnimation(string aniName)
     {
         print(aniName);
         aniplay1.SetTrigger(aniName);
-        aniplay2.SetTrigger(aniName);
+        aniplay3.SetTrigger(aniName);
 
     }
 
